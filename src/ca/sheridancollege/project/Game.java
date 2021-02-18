b126e4134088e0f4133e91be75cfeb7e5067beee
JaycobBond <jaycob.bond@gmail.com>
2021-02-18 18:45:32 -0500

    Revert Commit

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
  * The class that models your game. You should create a more specific
  * child of this class and instantiate the methods given.
  * @author dancye
- * Test test
  */
 public abstract class Game 
 {
